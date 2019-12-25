d85c73caec0961ecebf9073b452b09ad1c704113
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2019-12-26 02:02:26 +0900

    Update FinedustMonitorWithGPS.ino

--- FinedustMonitorWithGPS/FinedustMonitorWithGPS.ino
+++ FinedustMonitorWithGPS/FinedustMonitorWithGPS.ino
@@ -94,48 +94,110 @@ void loop() {
       yield(); 
     }
   }
-  while (dust.available() > 0) { 
+  while (dust.available() > 0) {
     do_dust(dust.read(), got_dust);
     yield();                                          //loop 에서 while 문을 사용하는 경우 yield 를 포함해주어야 합니다.
 
-    if (8 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 0 || 8 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 0){
+    if (8 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 0) {
+      pm25i = 1;
+    }
+    else if (16 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 9) {
+      pm25i = 2;
+    }
+    else if (26 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 17) {
+      pm25i = 3;
+    }
+    else if (34 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 27) {
+      pm25i = 4;
+    }
+    else if (35 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 43) {
+      pm25i = 5;
+    }
+    else if (51 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 44) {
+      pm25i = 6;
+    }
+    else if (60 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 52) {
+      pm25i = 7;
+    }
+
+    if (8 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 0) {
+      pm10i = 1;
+    }
+    else if (16 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 9) {
+      pm10i = 2;
+    }
+    else if (51 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 17) {
+      pm10i = 3;
+    }
+    else if (68 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 52) {
+      pm10i = 4;
+    }
+    else if (84 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 69) {
+      pm10i = 5;
+    }
+    else if (101 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 85) {
+      pm10i = 6;
+    }
+    else if (120 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 102) {
+      pm10i = 7;
+    }
+
+    if (pm25i >= pm10i) {
+      if (pm25i == 1) {
         status = "Excellent (1) : The air quality is excellent. The air pollution pose no threat. The conditions ideal for outdoor activities.";
-    }
-
-    else if (16 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 9 || 16 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 9){
-          status = "Very Good (2) : The air pollution pose minimal risk to exposed persons. Conditions very good for outdoor activities.";
-    }
-
-    else if (26 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 17 || 51 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 17){
-          status = "Good (3) : Air quality is acceptable. Air pollution can endanger people at risk. Conditions good for outdoor activities.";
-    }
-
-    else if (34 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 27 || 68 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 52){
-          status = "Satisfactory (4) : Air quality is average. The air pollution pose a threat for people at risk, which may experience health effects. Other people should limit spending time outdoors, especially when they experience symptoms such as cough or sore throat.";
-    }
-
-    else if (35 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 43 || 84 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 69){
-          status = "Bad (5) : Air quality is bad. People at risk should avoid to go outside. Not recommended for outdoor activities.";
-    }
-    
-    else if (51 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 44 || 101 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 85){
-          status = "Very Bad (6) : Air quality is very bad. People at risk should avoid to go outside. Not recommended for outdoor activities.";
-    }
-
-    else if (60 >= int(pm25s.getMedian()) && int(pm25s.getMedian()) >= 52 || 120 >= int(pm10s.getMedian()) && int(pm10s.getMedian()) >= 102){
-          status = "Hazardous (7) : The quality of air is worst and dangerous. People at risk should be avoided to go outside and should limit the outdoor activities to minimum. Outdoor activities are discouraged.";
+      }
+      else if (pm25i == 2) {
+        status = "Very Good (2) : The air pollution pose minimal risk to exposed persons. Conditions very good for outdoor activities.";
+      }
+      else if (pm25i == 3) {
+        status = "Moderate (3) : Air quality is acceptable. Air pollution can endanger people at risk. Conditions good for outdoor activities.";
+      }
+      else if (pm25i == 4) {
+        status = "Satisfactory (4) : Air quality is average. The air pollution pose a threat for people at risk, which may experience health effects. Other people should limit spending time outdoors, especially when they experience symptoms such as cough or sore throat.";
+      }
+      else if (pm25i == 5) {
+        status = "Bad (5) : Air quality is bad. People at risk should avoid to go outside. Not recommended for outdoor activities.";
+      }
+      else if (pm25i == 6) {
+        status = "Very Bad (6) : Air quality is very bad. People at risk should avoid to go outside. Not recommended for outdoor activities.";
+      }
+      else if (pm25i == 7) {
+        status = "Hazardous (7) : The quality of air is worst and dangerous. People at risk should be avoided to go outside and should limit the outdoor activities to minimum. Outdoor activities are discouraged.";
+      }
+    } else if (pm25i < pm10i) {
+      if (pm10i == 1) {
+        status = "Excellent (1) : The air quality is excellent. The air pollution pose no threat. The conditions ideal for outdoor activities.";
+      }
+      else if (pm10i == 2) {
+        status = "Very Good (2) : The air pollution pose minimal risk to exposed persons. Conditions very good for outdoor activities.";
+      }
+      else if (pm10i == 3) {
+        status = "Moderate (3) : Air quality is acceptable. Air pollution can endanger people at risk. Conditions good for outdoor activities.";
+      }
+      else if (pm10i == 4) {
+        status = "Satisfactory (4) : Air quality is average. The air pollution pose a threat for people at risk, which may experience health effects. Other people should limit spending time outdoors, especially when they experience symptoms such as cough or sore throat.";
+      }
+      else if (pm10i == 5) {
+        status = "Bad (5) : Air quality is bad. People at risk should avoid to go outside. Not recommended for outdoor activities.";
+      }
+      else if (pm10i == 6) {
+        status = "Very Bad (6) : Air quality is very bad. People at risk should avoid to go outside. Not recommended for outdoor activities.";
+      }
+      else if (pm10i == 7) {
+        status = "Hazardous (7) : The quality of air is worst and dangerous. People at risk should be avoided to go outside and should limit the outdoor activities to minimum. Outdoor activities are discouraged.";
+      }
     }
   }
 
   //Serial.println(map_x);
   //Serial.print("pm 25 : ");
   //Serial.println(int(pm25s.getMedian()));
-  
+
   if (millis() > mark) {//one minute(60000) interval
     mark = millis() + 60000;
     got_interval = true;
   }
-  
+
   if (got_interval) {
     got_interval = false;
     do_interval();
