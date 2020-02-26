89e0e74867f8f85747367665d731f99e324ed9fa
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2020-02-26 10:45:24 +0900

    Update ReverseGeocoding.vb

--- Maps/Excel/ReverseGeocoding.vb
+++ Maps/Excel/ReverseGeocoding.vb
@@ -82,6 +82,35 @@ Public Function ADDRGEOCODE(address As String) As String
     End If
 End Function
 
+        
+' Public Function URLEncode(StringVal As String, Optional SpaceAsPlus As Boolean = False) As String
+'   Dim StringLen As Long: StringLen = Len(StringVal)
+'
+'   If StringLen > 0 Then
+'     ReDim result(StringLen) As String
+'     Dim i As Long, CharCode As Integer
+'     Dim Char As String, Space As String
+'
+'     If SpaceAsPlus Then Space = "+" Else Space = "%20"
+'
+'     For i = 1 To StringLen
+'       Char = Mid$(StringVal, i, 1)
+'       CharCode = asc(Char)
+'       Select Case CharCode
+'         Case 97 To 122, 65 To 90, 48 To 57, 45, 46, 95, 126
+'           result(i) = Char
+'         Case 32
+'           result(i) = Space
+'         Case 0 To 15
+'           result(i) = "%0" & Hex(CharCode)
+'         Case Else
+'           result(i) = "%" & Hex(CharCode)
+'       End Select
+'     Next i
+'     URLEncode = Join(result, "")
+'   End If
+' End Function
+
 
 Public Function URLEncode(ByVal StringVal As String, Optional SpaceAsPlus As Boolean = False) As String
   Dim bytes() As Byte, b As Byte, i As Integer, space As String
