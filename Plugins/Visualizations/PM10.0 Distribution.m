a62d42cf36d2d90485615842421d19d205ddd9f9
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2020-07-03 13:37:36 +0900

    Update PM10.0 Distribution.m

--- Plugins/Visualizations/PM10.0 Distribution.m
+++ Plugins/Visualizations/PM10.0 Distribution.m
@@ -1,67 +1,55 @@
-% Enter your MATLAB code below
+% Read PM 10.0 value data from a ThingSpeak channel for three seperate days 
+% and visualize the data in a single plot using the PLOT function. 
 
-readChannelID = [ThingSpeak Channel ID];
+% Channel ID to read data from 
+readChannelID = [ThingSpeak Channel ID]; 
+% PM 10.0 Field ID 
+pm10FieldID = [ThingSpeak Field ID]; 
 
-readAPIKey = '[ThingSpeak Read API Key]';
+% Channel Read API Key 
+% If your channel is private, then enter the read API 
+% Key between the '' below: 
+readAPIKey = '[ThingSpeak Read API Key]'; 
 
-trailingdays = 7
+% Specify date range
+dateRange = [datetime('today')-days(6),datetime('now')];
+% Read data including the timestamp, and channel information.
+[data,time,channelInfo] = thingSpeakRead(readChannelID,'Fields',1:2,...
+                          'DateRange',dateRange);
+% Create variables to store different sorts of data
+pm25Data = data(:,1);
+pm10Data = data(:,2);
 
-[data,timestamps] = thingSpeakRead(readChannelID,'ReadKey',readAPIKey,'Fields',[2],'NumDays',trailingdays);
+% Create a day range vector
+dayRange = day(dateRange(1):dateRange(2));
+% Pre-allocate matrix
+AQData = zeros(length(dayRange),24);
 
-TT1 = array2timetable(data,'RowTimes',timestamps);
-TT2 = retime(TT1,'hourly','mean');
-
-mytime=(TT2.Time);
-mydata=(TT2.data);
-
-mytimeTZ = datetime(mytime, 'TimeZone','UTC');
-mytimeTZ.TimeZone = 'Asia/Seoul';
-
-myhour=hour(mytimeTZ)
-myday=day(mytimeTZ)
-
-h=zeros(24,trailingdays);
-
-t=datetime
-tTZ = datetime(t, 'TimeZone','UTC');
-tTZ.TimeZone = 'Asia/Seoul';
-
-tominus=(day(tTZ)-trailingdays-1)
-
-for i = 1:(length(myhour))
-x = int8((myhour(i))+1)
-y = int8((myday(i))-tominus);
-z = mydata(i);
-h(x,y) = z;
+% Generate PM 10.0 value 3D bar chart
+% Get PM 10.0 value per whole clock for each day
+for m = 1:length(dayRange) % Loop over all days
+    for n = 1:24 % Loop over 24 hours
+        if any(day(time)==dayRange(m) & hour(time)==n); % Check if data exist for this specific time
+            hourlyData = pm10Data((day(time)==dayRange(m) & hour(time)==n)); % Pull out the hourly PM 10.0 value from the matrix
+            AQData(m,n) = hourlyData(1); % Assign the PM 10.0 value at the time closest to the whole clock
+        end
+    end
 end
 
-for i = 1:trailingdays+1
-[DayNumber, DayName] = weekday(t-trailingdays-1+i)
-xnames{i}=DayName
+% Plot
+figure
+h = bar3(datenum(dateRange(1):dateRange(2)), AQData);
+for k = 1:length(h) % Change the face color for each bar
+    h(k).CData = h(k).ZData;
+    h(k).FaceColor = 'interp';
 end
-
-bar3(h)
-
-Z = magic(5);
-b = bar3(h);
-colorbar
-
-for h = 1:length(b)
-    zdata = b(h).ZData;
-    b(h).CData = zdata;
-    b(h).FaceColor = 'interp';
-end
-
-az = 27.5;
-el = 20.5;
-view(az, el);
-ylim([0 25])
-ynames = {'06:00'; '12:00'; '18:00'; '00:00'};
-set(gca,'ytick',[2 8 14 20],'yticklabel',ynames)
-set(gca,'xtick',[1:trailingdays+1],'xticklabel',xnames)
-xtickangle(90)
-
-title(['PM 10.0 Distribution (',num2str(trailingdays),' days trailing)'])
-xlabel('Days')
-ylabel('Time (UTC)')
-zlabel('PM 10.0')
+title('PM 10.0 Distribution')
+xlabel('Hour of Day')
+ylabel('Days')
+datetick('y','ddd') % Change the Y-Tick to display specified date format
+% datetick('y','mmm dd')
+ax = gca;
+ax.XTick = 1:24
+ax.XTickLabels = 1:24
+ax.YTickLabelRotation = 30; % Rotate label for better display
+colorbar % Add a color bar to indicate the scaling of color
