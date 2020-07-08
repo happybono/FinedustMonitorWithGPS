c4671ff68acbca80c12974cfed6a6e19dbd87043
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2020-07-08 13:10:59 +0900

    Update server.ino

--- FinedustMonitorWithGPS/server.ino
+++ FinedustMonitorWithGPS/server.ino
@@ -53,42 +53,46 @@ void do_server_plaive(String api_key,int pm25, int pm10, float temperature, Stri
 }
 
 void do_server_thingspeak(String api_key,int pm25, int pm10, float temperature, String map_x, String map_y, String status) {
-
-  data = "api_key=" + String(api_key) + "&field1=" + String(pm25) + "&field2=" + String(pm10) + "&field3=" + String(temperature) + "&field4=" + String(map_x) + "&field5=" + String(map_y) + "&status=" + String(status);
-  //contentType= "application/x-www-form-urlencoded";
-
-
-  //서버 통신 공식 client.println 을 사용하여야 합니다.
-
-  /*  Write Data with Get :
-   *  https://www.mathworks.com/help/thingspeak/writedata.html
-   *
-   *  Usage : 
-   *  https://api.thingspeak.com/update?api_key=<write_api_key>&field1=123 */
-     
   if(client.connect(host_thingspeak, httpPort)){
-    Serial.println("connected");
+    data = "api_key=" + String(api_key) + "&field1=" + String(pm25) + "&field2=" + String(pm10) + "&field3=" + String(temperature) + "&field4=" + String(map_x) + "&field5=" + String(map_y) + "&status=" + String(status);
+    //Serial.println("data = " + String(data));
+    //Serial.println("connected");
     client.print("GET /update?");
     client.print(data); 
     client.println(" HTTP/1.1");
-    client.println("Host: " + String(host_thingspeak)); // SERVER ADDRESS HERE AS WELL
+    client.println("Host: " + String(host_thingspeak));
     client.println("Cache-Control: no-cache");
-    
-    //client.println("Content-Type: application/xE-www-form-urlencoded"); 
-    //client.print("Content-Length: "); 
-    //client.println(data.length()); 
     client.println("Connection: close");
     client.println();
-     
-    //client.print(data); 
-  }
     
-  //서버 통신이 되지 않으면
-  else {
+    //String answer = getResponse();
+    //Serial.println("response = " + String(answer));
+  } else {
     Serial.println("connection failed: ");
     return;
   }
 }
+
+//String getResponse(){
+//  String response;
+//  long startTime = millis();
+//
+//  delay( 200 );
+//  while ( client.available() < 1 ){
+//        delay( 5 );
+//  }
+//  
+//  if( client.available() > 0 ){ // Get response from server.
+//     char charIn;
+//     do {
+//         charIn = client.read(); // Read a char from the buffer.
+//         response += charIn;     // Append the char to the string response.
+//        } while ( client.available() > 0 );
+//    }
+//  client.stop();
+//        
+//  return response;
+//}
 
 void do_server_default(String api_key, int pm25, int pm10, float temperature, String map_x, String map_y) {
 
@@ -108,6 +112,7 @@ void do_server_default(String api_key, int pm25, int pm10, float temperature, St
     client.println(data.length()); 
     client.println("Connection: close");
     client.println(); 
+    //Serial.println(data);
     //client.print(data); 
   }
     
