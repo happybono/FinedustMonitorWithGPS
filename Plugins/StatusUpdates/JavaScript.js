4d0d9d41fd856829c4b3250cab95d10809e047eb
Charlie Jaewoong Mun <40027494+happybono@users.noreply.github.com>
2020-07-28 09:56:08 +0900

    Update JavaScript.js

--- Plugins/StatusUpdates/JavaScript.js
+++ Plugins/StatusUpdates/JavaScript.js
@@ -8,13 +8,13 @@ var api_key = '[ThingSpeak Read API Key]';
 
 // variable for the data point
 var p_pm10,
-  p_pm25,
-  p_temp,
-  p_lat,
-  p_lng,
-  p_address,
-  p_timestamp,
-  timestamp;
+    p_pm25,
+    p_temp,
+    p_lat,
+    p_lng,
+    p_address,
+    p_timestamp,
+    timestamp;
 
 function initPage() {
   loadData();
@@ -72,9 +72,9 @@ function outputData() {
   // update page
   document.getElementById('pm25').innerHTML = p_pm25 + ' ㎍ / ㎥ (' + (p_pm25 / 20 * 100).toFixed(0) + '%)';
   document.getElementById('pm10').innerHTML = p_pm10 + ' ㎍ / ㎥ (' + (p_pm10 / 50 * 100).toFixed(0) + '%)';
-  document.getElementById('temperature').innerHTML = p_temp.toFixed + ' °C';
+  document.getElementById('temperature').innerHTML = p_temp + ' °C';
   document.getElementById('latlng').innerHTML = p_lat.toFixed(6) + '˚, ' + p_lng.toFixed(6) + '˚';
-  document.getElementById('address').innerHTML = p_address;
+  document.getElementById('address').innerHTML = p_address.link("http://google.com/maps/place/?q=" + p_address, "_blank" + "</p>");
   document.getElementById('timestamp').innerHTML = '@ ' + timestamp;
 }
 
